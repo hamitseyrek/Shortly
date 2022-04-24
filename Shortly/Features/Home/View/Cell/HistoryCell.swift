0af678423928fc97c866c6cbcaa3521254ab9818
hamitseyrek <hamitseyrek@gmail.com>
2022-04-25 02:39:27 +0300

    designed HistoryCell

--- Shortly/Features/Home/View/Cell/HistoryCell.swift
+++ Shortly/Features/Home/View/Cell/HistoryCell.swift
@@ -9,14 +9,18 @@ import UIKit
 
 class HistoryCell: UITableViewCell {
 
+    @IBOutlet weak var whiteView: UIView!
+    @IBOutlet weak var copyButton: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
+        whiteView.layer.cornerRadius = 10
+        copyButton.layer.cornerRadius = 5
         // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
--- Shortly/Features/Home/View/HomeVC.swift
+++ Shortly/Features/Home/View/HomeVC.swift
@@ -10,13 +10,16 @@ import UIKit
 class HomeVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var shortenButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        shortenButton.layer.cornerRadius = 5
+        
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(UINib(nibName: "WelcomeCell", bundle: Bundle.main), forCellReuseIdentifier: "Cell")
+        tableView.register(UINib(nibName: "HistoryCell", bundle: Bundle.main), forCellReuseIdentifier: "HCell")
         tableView.contentInsetAdjustmentBehavior = .never
         
         
@@ -34,19 +37,40 @@ class HomeVC: UIViewController {
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? WelcomeCell else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HCell", for: indexPath) as? HistoryCell else {
             fatalError("Error dequing cell: MovieCell")
         }
-        
+        cell.layer.cornerRadius = 20
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        self.tableView.frame.height
+        200
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        
+        let label = UILabel()
+        label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
+        label.text = "Your Link History"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .gray
+        label.textAlignment = .center
+        
+        
+        headerView.addSubview(label)
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
 }
