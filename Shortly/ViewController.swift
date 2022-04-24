0e921797539063bd8e97c0c87adbe5f0769eb261
hamitseyrek <hamitseyrek@gmail.com>
2022-04-24 23:56:40 +0300

    Initial Commit

---
+++ Shortly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shortly
+//
+//  Created by Hamit Seyrek on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
